baaa4348d2a7130225c141912f0677cb4668b07e
Ruud Goorden <ruudgoorden@ruuds-mbp.home>
2023-07-22 11:11:39 +0200

    ingestion added

--- ingestion.py
+++ ingestion.py
@@ -19,8 +19,26 @@ output_folder_path = config['output_folder_path']
 #############Function for data ingestion
 def merge_multiple_dataframe():
     #check for datasets, compile them together, and write to an output file
+    df = pd.DataFrame()
+    files = []
 
+    for file in os.listdir(input_folder_path):
+        file_path = os.path.join(input_folder_path, file)
+        df_input = pd.read_csv(file_path)
+        df = df.append(df_input, ignore_index=True)
 
+        files.append(file_path)
+
+    df.drop_duplicates(inplace=True)
+    
+    # Write data to csv
+    df.to_csv(os.path.join(output_folder_path, 'finaldata.csv'), index=False)
+
+    # Write data with timestamp
+    with open(os.path.join(output_folder_path, 'ingestedfiles.txt'), "w") as file: 
+        file.write(
+            f"Date of ingestion: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}\n")
+        file.write("\n".join(files))
 
 if __name__ == '__main__':
     merge_multiple_dataframe()
